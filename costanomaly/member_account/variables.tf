0649400bd65d93851e1808bf16741f478200e08c
Adewunmi Ibironke <dewheart@outlook.com>
2024-05-02 16:41:29 -0400

    Adding cost anomaly tf template for single or member account(s)

---
+++ costanomaly/member_account/variables.tf
@@ -0,0 +1,29 @@
+variable "AWSAccountName" {
+  description = "This is the name of the AWS account that cost anomaly is being set for"
+  type        = string
+#  default     = ""
+}
+
+variable "Email" {
+  description = "This is the email of Cloud Architecture Team that will be alerted"
+  type        = string
+#  default     = ""
+}
+
+variable "CustomerEmail" {
+  description = "This is the email of the customer that will be alerted"
+  type        = string
+#  default     = ""
+}
+
+variable "Total_Impact_Absolute" {
+  description = "This is the set amount to alert against, can be budget amount"
+  type        = list(string)
+  default     = ["100"]
+}
+
+variable "Total_Impact_Percentage" {
+  description = "This is the percentage against true AWS spend"
+  type        = list(string)
+  default     = ["10"]
+}
